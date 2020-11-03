40a76c955efd8b02258f078f9646837ea0b27246
Douglas1912 <d.andre070@gmail.com>
2020-11-03 11:49:44 +0100

    Deluppgift 3, Konfigurerat ADC som läser in värdet från en potetiometer i porten A0 som senare skall skickas till en LED kopplat i porten OCR0A, timer0 satt till fast PWM, Konfigurerat timer2 i CTC-mode med avbrotshanterare som startar ADC omvandligen för att sedan skickade det värdet sparat i en global variabel till OCR0A

--- main.c
+++ main.c
@@ -7,13 +7,97 @@
 #include "serial.h"
 #include "timer.h"
 
+int readValueADC = 0; // define an integer to save adc read value
+uint8_t duty = 0;
+
+unsigned int ADC_read(unsigned char chnl);
+void init_ADC();
+void set_PWM_Output(uint8_t duty);
+
+
+int main(void)
+{
+	uart_init();
+	timer_init();
+	timer2_init();
+	LED_init();
+	init_ADC();
+
+	while (1)
+	{
+
+	}
+	return 0;
+}
+
+
+void init_ADC()
+{
+	ADMUX = (1 << REFS0); // Selecting internal reference voltage For Aref=AVcc
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Enable ADC also set Prescaler as 128
+
+}
+
+
+unsigned int ADC_read(unsigned char chnl)
+{
+	ADCSRA=(1<<ADEN)|(1<<ADPS1)|(1<<ADPS0); //Enable ADC with Prescalar=Fcpu/128 
+
+	chnl = chnl & 0b00000111; // select adc channel between 0 to 7
+	ADMUX = 0x40;        //channel A0 selected
+	ADMUX |= (1<<REFS0)|(1<<REFS1); //set reference voltage to 5v (AREF)
+	ADCSRA |= (1 << ADSC);   // start conversion
+	while (!(ADCSRA & (1 << ADIF)));   // wait for ADIF conversion complete return
+	ADCSRA |= (1 << ADIF);   // clear ADIF when conversion complete by writing 1
+	return (ADC); //return calculated ADC value
+}
+
+void set_PWM_Output(uint8_t duty)
+{
+   OCR0A=duty;
+}
+
+ISR(TIMER2_COMPA_vect)
+{
+	
+	printf("%d\n", readValueADC);
+
+	readValueADC = ADC_read(0);   //save adc read value in integer
+	
+	if (readValueADC >= 256)
+	{
+		duty = 255;
+		set_PWM_Output(duty);
+	}
+
+	else if (readValueADC <= 255)
+	{
+		duty = readValueADC;
+		set_PWM_Output(duty);
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Delluppgift 2
+/*
 #define BIT_IS_SET(byte, bit) (byte & (1 << bit))
 
 uint8_t buttonState = 0; 
 int buttonPressed = 0;
 int pressedConfidenceLevel = 0; //Measure button press cofidence
 int releasedConfidenceLevel = 0; //Measure button release confidence
-
 
 int main(void) {
 
@@ -54,4 +138,4 @@ int main(void) {
 		}
 	}
 	return 0;
-}
+}*/
--- timer.c
+++ timer.c
@@ -9,16 +9,16 @@ void timer_init() {
 
     TCCR0A |= (1 << WGM01) | (1 << WGM00);
     // set fast PWM Mode
-
 	
     TCCR0B |= (1 << CS01)|(1 << CS00);
-    // set prescaler to 64 and starts PWM
+    // set prescaler to 64 and starts PWM, 255 0xFF as TOP
 }
 
 void timer2_init(void) { 
 
-	TCCR2A |= (1 << WGM21) ; 
-	// set timer 2 for CTC mode
+	TCCR2A |= (1 << WGM21); // set timer 2 for CTC mode (TOP = OCR2A)
+	
+	//TCCR2A |= (1 << COM2A0); // Clear output pin OC2A on compare match PIN11 Arduino Uno
 	
 	TCCR2B |= ((1 << CS22) | (1 << CS21) | (1 << CS20));
     // set prescaler to 1024 
@@ -31,12 +31,15 @@ void timer2_init(void) {
 	//Divide the Result though inverse of the desired milli-frequency (16ms): 
 	//15625/((1/16)*1000) = 250 (OCR2A = 250)
 	
-	OCR2A = 195;
-	// initialize compare value
+	TIMSK2 |= (1 << OCIE2A);  // Enable Timer 2 Output Compare Match Interrupt for channel A
+	TIFR2 |= (1 << TOV2) | (1<<OCF2A); //clear pending interrupt flag for channel A
+ 
 	
-	TIMSK2 |= (1 << OCIE2A);
+	OCR2A = 156; // Set Output Compare Value to compare match register 16MHz/1024/100Hz
 	
-	sei();
+	TCNT2  = 0; // initialize counter
+
+	sei(); // Enable interrupts globally
 	
    
 }
